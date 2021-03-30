ca90f43887aead69718bfd669fa77a6df1583eb1
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-03-30 11:06:26 -0500

    Added Subject model

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_160350) do
+ActiveRecord::Schema.define(version: 2021_03_30_160545) do
 
   create_table "courses", force: :cascade do |t|
     t.string "title"
     t.integer "number"
     t.string "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "subjects", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
