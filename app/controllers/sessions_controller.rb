280c5d00ddd05887351dddc4f1081d4482e72845
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-03-30 15:25:15 -0500

    Deleted bootstrap theme, was having too many difficulties

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,13 @@
 class SessionsController < ApplicationController
 
-  
+  def home
+  end
+
+  def destroy
+    if session[:user_id]
+      session.delete :user_id
+    end
+    redirect_to root_path
+  end
 
 end
