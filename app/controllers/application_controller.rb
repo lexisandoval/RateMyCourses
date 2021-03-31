922c92689cef1a2147b286ff13bfae9eb8e3a381
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-03-31 12:50:25 -0500

    Flash message added for unauthorized users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,20 +16,8 @@ class ApplicationController < ActionController::Base
   def redirect_if_not_logged_in
     if !logged_in?
       flash[:error] = "You must be logged in to view this page."
-      redirect '/'
+      redirect_to '/'
     end
   end
-
-  # def authorized_edit?(sale)
-  #   sale.user == current_user
-  # end
-
-  # def redirect_if_not_authorized(sale)
-  #   if !authorized_edit?(sale)
-  #     flash[:error] = "You may only review your own sales."
-  #     redirect '/'
-  #   end
-  # end
-
 
 end
