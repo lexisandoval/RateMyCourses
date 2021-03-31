a28a43fd2dfb1afaf033c4462762f904a84bdeec
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-03-31 12:13:33 -0500

    Added login functionality and flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,21 @@
 class SessionsController < ApplicationController
 
-  def home
-  end
-
   def destroy
     if session[:user_id]
       session.delete :user_id
     end
     redirect_to root_path
+  end
+
+  def create 
+    user = User.find_by(username: params[:user][:username])
+    if user && user.authenticate(params[:user][:password])
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      flash[:error] = "Login failed: The information provided was incorrect."
+      redirect_to login_path
+    end
   end
 
 end
