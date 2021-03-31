a9f711cff80b106f9d4a7d4bc98bcd7cca3ac7e3
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-03-31 15:26:25 -0500

    Updated nav bar to include hhome reference

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,13 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#destroy'
   
   resources :ratings
-  resources :subjects
-  resources :courses
-  resources :users
+  # resources :subjects
+  resources :courses do
+    resources :ratings, only: [:new, :create, :index]
+  end
+
+  resources :users do 
+    resources :ratings, only: [:new, :create, :index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
