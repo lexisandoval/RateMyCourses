9c4b428c563b1e4ba7cd1266d4a2ac3c0ed2d711
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-04-01 12:47:44 -0500

    Added course validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,12 @@ class Course < ApplicationRecord
   has_many :users, through: :ratings
   belongs_to :subject
 
+  validates :subject_id, :number, :title, :description, presence: true
+  validates :number, length: { is: 3}
+  validates :number, numericality: { only_integer: true }
+  validates :title, length: { in: 3..25 }
+  validates :description, length: { in: 10..100 }
+
   def number_with_subject
     "#{self.subject.name} #{self.number}"
   end
