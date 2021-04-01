5192f199a3a40e45a175a647b809b18347fefe14
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-04-01 10:15:13 -0500

    Added edit route for ratings

--- config/routes.rb
+++ config/routes.rb
@@ -15,7 +15,7 @@ Rails.application.routes.draw do
   resources :courses
 
   resources :users do 
-    resources :ratings, only: [:new, :create, :index, :show]
+    resources :ratings, only: [:new, :create, :index, :show, :edit]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
