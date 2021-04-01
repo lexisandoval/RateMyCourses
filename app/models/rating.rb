a6ddf3e9636c3b5bd0cf769454754ddc3abbea54
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-04-01 12:30:15 -0500

    Added simple ratings validations

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -3,6 +3,10 @@ class Rating < ApplicationRecord
   belongs_to :user
   belongs_to :course
 
+  validates :course_id, presence: true
+  validates :stars, presence: true
+  validates :content, presence: true
+
   def content_with_author
     "#{self.content} - #{self.user.name.capitalize}"
   end
