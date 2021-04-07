99dd4ca7db539b79d5c752ba7c46a9dd38cfcff5
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-04-07 10:16:53 -0500

    Removed commented code

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,11 @@ Rails.application.routes.draw do
   root 'sessions#home'
 
   get '/signup' => 'users#new'
-  # get '/signup' => 'users#create'
-
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   delete '/logout' => 'sessions#destroy'
   
   resources :ratings
-  # resources :subjects
   resources :courses
 
   resources :users do 
