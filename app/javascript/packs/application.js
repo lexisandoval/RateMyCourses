530d115c65cde8ac629a6c07810df8db05b61fe8
Lexi Sandoval <lexi.sandoval9@gmail.com>
2021-04-08 10:19:11 -0500

    Updated styling for mobile

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,7 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+if(window.innerHeight > window.innerWidth){
+  document.getElementsByTagName("body")[0].style.transform = "rotate(90deg)";
+}
